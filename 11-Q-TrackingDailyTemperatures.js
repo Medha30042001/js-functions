613c60aa57360eee7436960f21e3f3e313dda996
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:37:11 +0530

    Create 11-Q-TrackingDailyTemperatures.js

---
+++ 11-Q-TrackingDailyTemperatures.js
@@ -0,0 +1,13 @@
+/*
+
+Assignment : Let’s Reuse Code: Functions That Work for You
+
+L0 - Array Manipulation: Tracking Daily Temperatures
+
+Problem Statement:
+Create an array that stores the temperatures recorded over 7 days. Use indexing to access the temperature for the 4th day and update it to a new value by adding 2 degrees. After that, log the updated temperatures for all 7 days to the console.
+
+Submission Guidelines:
+Please submit your Masai Git directory link/ One compiler link
+
+*/
