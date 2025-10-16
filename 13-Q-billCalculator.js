c4f7b05dda6ab54a82e9de1c2ee427a3a5d1e36b
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:42:58 +0530

    Create 13-Q-billCalculator.js

---
+++ 13-Q-billCalculator.js
@@ -0,0 +1,21 @@
+/*
+Q: 13
+Smart Energy Bill Calculator
+Write a function named calculateEnergyBill that calculates the monthly electricity bill based on the following rules:
+
+For the first 100 units, the cost is Rs. 5 per unit.
+For the next 100 units (101 to 200), the cost is Rs. 8 per unit.
+For units above 200, the cost is Rs. 10 per unit.
+The function should take one parameter:
+
+unitsConsumed (a number): The total units of electricity consumed.
+Return the total bill amount in rupees.
+
+Example:
+
+calculateEnergyBill(50);   // Should return 250
+calculateEnergyBill(150);  // Should return 900
+calculateEnergyBill(250);  // Should return 1900
+Submission Guidelines
+Once Completed, submit the code directly in the box below
+*/
